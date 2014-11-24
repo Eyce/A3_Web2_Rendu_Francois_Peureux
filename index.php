0588f1351d4b724d1c48debcfb2b28eacd485c01
Eyce <peureuxf@gmail.com>
2014-11-24 12:55:21 +0100

    Updated integer validation

--- index.php
+++ index.php
@@ -10,6 +10,9 @@ require __DIR__.'/vendor/autoload.php';
 echo $validation = RenduFP\validatorLibrary\IntegerValidator::equals(8, 8).PHP_EOL;
 echo $validation = \RenduFP\validatorLibrary\IntegerValidator::biggerThan(5, 8).PHP_EOL;
 echo $validation = \RenduFP\validatorLibrary\IntegerValidator::smallerThan(5, 8).PHP_EOL;
+echo $validation = \RenduFP\validatorLibrary\IntegerValidator::between(5, 1, 10).PHP_EOL;
+echo $validation = RenduFP\validatorLibrary\IntegerValidator::isPositive(-5).PHP_EOL;
+echo $validation = RenduFP\validatorLibrary\IntegerValidator::isNegative(0,RenduFP\validatorLibrary\IntegerValidator::NOT_STRICT_INEQUALITY).PHP_EOL;
 
 /*
  * Boolean Validation
--- src/IntegerValidator.php
+++ src/IntegerValidator.php
@@ -11,6 +11,9 @@ namespace RenduFP\validatorLibrary;
 
 class IntegerValidator {
 
+    const STRICT_INEQUALITY     = 0;
+    const NOT_STRICT_INEQUALITY = 1;
+
     public function __construct()
     {
 
@@ -40,4 +43,47 @@ class IntegerValidator {
             return "False";
     }
 
+    public static function between($intValue, $testValueMin, $testValueMax)
+    {
+        if($intValue>$testValueMin && $intValue<$testValueMax)
+            return "True";
+        else
+            return "False";
+    }
+
+    public static function isPositive($intValue, $strictness = self::STRICT_INEQUALITY)
+    {
+        if ($strictness = self::STRICT_INEQUALITY)
+        {
+            if($intValue>0)
+                return "True";
+            else
+                return "False";
+        }
+        else if($strictness = self::NOT_STRICT_INEQUALITY)
+        {
+            if($intValue>=0)
+                return "True";
+            else
+                return "False";
+        }
+    }
+
+    public static function isNegative($intValue, $strictness = self::STRICT_INEQUALITY)
+    {
+        if ($strictness = self::STRICT_INEQUALITY)
+        {
+            if($intValue<0)
+                return "True";
+            else
+                return "False";
+        }
+        else if($strictness = self::NOT_STRICT_INEQUALITY)
+        {
+            if($intValue<=0)
+                return "True";
+            else
+                return "False";
+        }
+    }
 } 
